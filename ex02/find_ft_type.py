1fdc79a603d8779bc95eef20c818eb0d9e6d2123
nargin <mejumine@gmail.com>
2024-01-21 01:08:40 +0100

    5 out of 10 exercices

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,22 @@
+
+
+def all_thing_is_obj(object: any) -> int :
+	obj_type = object.__class__.__name__
+	content = object
+	object = type(object)
+	match obj_type :
+		case "str" :
+			print(f"{content} is in the kitchen: {object}")
+		case "list" :
+			print("List", object)
+		case "tuple" :
+			print("Tuple", object)
+		case "set" :
+			print("Set", object)
+		case "dict" :
+			print("Dict", object)
+		case "NoneType" :
+			print("None")
+		case _ :
+			print("Type not found")
+	return 42
