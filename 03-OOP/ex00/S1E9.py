fcce2e332f7ab1e3c1116e546ca59ab0aa0ee44e
nargin <mejumine@gmail.com>
2024-02-07 11:44:03 +0100

    tested this time

--- 03-OOP/ex00/S1E9.py
+++ 03-OOP/ex00/S1E9.py
@@ -3,15 +3,19 @@ from abc import ABC, abstractmethod
 class Character(ABC):
 	"""Character class name: str, is_alive: bool"""
 	name = ""
-	alive = True
+	is_alive = True
 
 	@abstractmethod
 	def die(self):
+		"""Mort"""
 		pass
 	def __init__(self, name, alive=True):
+		"""init la"""
 		self.name = name
-		self.alive = alive		
+		self.is_alive = alive		
 
 class Stark(Character):
+	"""Docstring au top :D"""
 	def die(self):
+		"""Mort 2"""
 		self.alive = False
