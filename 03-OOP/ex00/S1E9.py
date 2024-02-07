5028e2d69ed209f8ed0e777d587e62897bd6aea2
nargin <mejumine@gmail.com>
2024-02-07 11:40:16 +0100

    abstract hmm ?

--- 03-OOP/ex00/S1E9.py
+++ 03-OOP/ex00/S1E9.py
@@ -2,9 +2,16 @@ from abc import ABC, abstractmethod
 
 class Character(ABC):
 	"""Character class name: str, is_alive: bool"""
+	name = ""
+	alive = True
+
 	@abstractmethod
 	def die(self):
-		
-		
+		pass
+	def __init__(self, name, alive=True):
+		self.name = name
+		self.alive = alive		
 
 class Stark(Character):
+	def die(self):
+		self.alive = False
