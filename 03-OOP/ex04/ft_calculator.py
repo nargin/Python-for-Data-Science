a08cef85bf49dd33cb87ed23bdc4c9524a55fcb4
nargin <92982124+nargin@users.noreply.github.com>
2024-09-09 14:43:33 +0200

    Decorator: Validation vector

--- 03-OOP/ex04/ft_calculator.py
+++ 03-OOP/ex04/ft_calculator.py
@@ -1,8 +1,6 @@
-class calculator:
-    """Vector calculator"""
-
-    @staticmethod
-    def __validate_vectors(V1: list[float], V2: list[float]) -> None:
+def validate_vectors(func):
+    """Decorator to validate the vectors"""
+    def wrapper(V1: list[float], V2: list[float]):
         """Validate the vectors"""
         if not isinstance(V1, list) or not isinstance(V2, list):
             raise ValueError("Vectors must be lists")
@@ -12,21 +10,30 @@ class calculator:
             raise ValueError("Vectors must contain only numbers")
         if len(V1) != len(V2):
             raise ValueError("Vectors must have the same length")
+        return func(V1, V2)
+    return wrapper
+
+
+class calculator:
+    """Vector calculator"""
 
     @staticmethod
+    @validate_vectors
     def dotproduct(V1: list[float], V2: list[float]) -> None:
         """Return the dot product of two vectors"""
-        calculator.__validate_vectors(V1, V2)
+        # calculator.__validate_vectors(V1, V2)
         print(sum([V1[i] * V2[i] for i in range(len(V1))]))
 
     @staticmethod
+    @validate_vectors
     def add_vec(V1: list[float], V2: list[float]) -> None:
         """Return the sum of two vectors"""
-        calculator.__validate_vectors(V1, V2)
+        # calculator.__validate_vectors(V1, V2)
         print([V1[i] + V2[i] for i in range(len(V1))])
 
     @staticmethod
+    @validate_vectors
     def sous_vec(V1: list[float], V2: list[float]) -> None:
         """Return the difference of two vectors"""
-        calculator.__validate_vectors(V1, V2)
+        # calculator.__validate_vectors(V1, V2)
         print([V1[i] - V2[i] for i in range(len(V1))])
